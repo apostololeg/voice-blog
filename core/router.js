db16e46c32f2bd433ca43e1b016a894e5fd5c3bf
Oleg Apostol <apostol@yandex-team.ru>
2014-10-16 01:36:10 +0400

    0.1.0

--- core/router.js
+++ core/router.js
@@ -1,4 +1,5 @@
 var fs = require('vow-fs'),
+    config = require('../.config.json'),
     getVoice = require('get-voice');
 
 function Router() {}
@@ -9,20 +10,21 @@ Router.prototype.index = function(req, res) {
 
 Router.prototype.say = function(req, res) {
     var q = req.query,
-        file = '/posts/'+q.title+'.mp3';
+        file = q.title + '.mp3';
 
-    console.log('\n[say]', q.title+':', q.text);
+    console.log('\n[say]', q.title + ':', q.text);
 
-    getVoice(q.text, function(err, buf){
+    getVoice(q.text, q.l || 'ru', function(err, buf){
         if(err) {
             console.log('ERROR', err);
         } else {
+            console.log('write to', file);
             // write audio to mp3
-            fs.write('.'+file, buf)
+            fs.write('./' + config.staticDir + '/posts/' + file, buf)
                 .then(function() {
                     console.log('   ===> saved to', file);
                     // open file in browser
-                    res.redirect(file);
+                    res.send(/posts/ + file);
                 });
         }
     });
